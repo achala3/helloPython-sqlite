9a2864c1459edc363aea89ec82ac073094437ec9
achala3 <130837566+achala3@users.noreply.github.com>
2023-04-24 22:36:57 +0900

    Update testDB.py

--- testDB.py
+++ testDB.py
@@ -1,10 +1,15 @@
 import  sqlite3
 conn=sqlite3.connect('C:/mycode/venvWorkSPace/tst.db')
-# conn=sqlite3.connect('tst.db')
-cur=conn.cursor()
-cur.execute("select * from students")
-# cur.execute(".tables")
-rows=cur.fetchall()
-for row in rows:
-    print(row)
-conn.close()
+
+with conn:   #conn.close()를 내포한  with문
+    
+    cur = conn.cursor()
+    sql="select * from score where 이름=? or 성적<=?"
+    # precompiled 구문 -> 반복 실행시 속도를 높여준다
+
+
+    cur.execute(sql,('홍길동',53))
+    rows = cur.fetchall()
+    
+    for row in rows:
+        print(row)
